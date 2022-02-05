210419dce55e3e8c0d7e20ebaae98cea6e25ac5d
dango3019 <2628422477@qq.com>
2022-02-05 14:22:25 +0800

    modify templates

--- FunTest/FunTest/settings.py
+++ FunTest/FunTest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-74skf)s#*ksk58z#n#7ljrkbgga#g_l9x*z5d*gtw*x1+i3(jw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
